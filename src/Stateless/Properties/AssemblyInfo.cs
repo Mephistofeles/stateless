d1e550618d8146e9919e7a8413dbbca25e3c1a88
Paweł Słowik <pawel-slowik@outlook.com>
2016-05-27 00:35:53 +0200

    Updated tests to use NUnitLite 3.2.1

---
+++ src/Stateless/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Stateless")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("<Not Specified>")]
+[assembly: AssemblyProduct("Stateless")]
+[assembly: AssemblyCopyright("Copyright © Stateless Contributors 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: InternalsVisibleTo("Stateless.Tests")]
+
+[assembly: NeutralResourcesLanguage("en")]
