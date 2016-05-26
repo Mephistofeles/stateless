aea8522d70cd9832f8ae99039f95594e735a9e4b
Paweł Słowik <pawel-slowik@outlook.com>
2016-05-27 01:25:49 +0200

    Updated examples to use .NET Core

---
+++ src/BugTrackerExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BugTrackerExample
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var bug = new Bug("Incorrect stock count");
+
+            bug.Assign("Joe");
+            bug.Defer();
+            bug.Assign("Harry");
+            bug.Assign("Fred");
+            bug.Close();
+
+            Console.ReadKey(false);
+        }
+    }
+}
---
+++ src/OnOffExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OnOffExample
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                string on = "On", off = "Off";
+                var space = ' ';
+
+                var onOffSwitch = new StateMachine<string, char>(off);
+
+                onOffSwitch.Configure(off).Permit(space, on);
+                onOffSwitch.Configure(on).Permit(space, off);
+
+                Console.WriteLine("Press <space> to toggle the switch. Any other key will raise an error.");
+
+                while (true)
+                {
+                    Console.WriteLine("Switch is in state: " + onOffSwitch.State);
+                    var pressed = Console.ReadKey(true).KeyChar;
+                    onOffSwitch.Fire(pressed);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
