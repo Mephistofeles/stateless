42612732f8b107e8a3ee3f8a86cffcd4e63ccc31
Paweł Słowik <pawel-slowik@outlook.com>
2016-05-27 00:09:42 +0200

    Updated AssemblyInfo and version number to 3.0.0

--- Stateless/Properties/AssemblyInfo.cs
+++ Stateless/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Stateless")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("<Not Specified>")]
 [assembly: AssemblyProduct("Stateless")]
+[assembly: AssemblyCopyright("Copyright © Stateless Contributors 2016")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
