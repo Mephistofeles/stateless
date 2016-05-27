95eedd5c95f9040107137a20d0c7e9645087bd0a
Paweł Słowik <pawel-slowik@outlook.com>
2016-05-27 02:00:00 +0200

    Cleaned up usings

--- test/Stateless.Tests/Properties/AssemblyInfo.cs
+++ test/Stateless.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -9,11 +7,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Stateless.Tests")]
 [assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("68a60677-a129-462a-b7b7-81b6eec43489")]
